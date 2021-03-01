078928cee60606baa4164257017943fbbc8167be
Alexandru Crisan <calex2005cj@gmail.com>
2021-03-01 21:43:08 +0200

    added files

---
+++ screens/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace Auto_Clicker {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing) {
+            if ( disposing && (components != null) ) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.CB_list = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_start.Location = new System.Drawing.Point(214, 130);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(91, 36);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_stop.Location = new System.Drawing.Point(214, 183);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(91, 38);
+            this.btn_stop.TabIndex = 1;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // CB_list
+            // 
+            this.CB_list.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.CB_list.CheckOnClick = true;
+            this.CB_list.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CB_list.FormattingEnabled = true;
+            this.CB_list.Items.AddRange(new object[] {
+            "Left Click",
+            "Right Click"});
+            this.CB_list.Location = new System.Drawing.Point(12, 119);
+            this.CB_list.Name = "CB_list";
+            this.CB_list.Size = new System.Drawing.Size(178, 112);
+            this.CB_list.TabIndex = 2;
+            this.CB_list.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "F9 : START";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(158, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "F10 : STOP";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(347, 285);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CB_list);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Auto Clicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.CheckedListBox CB_list;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ screens/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Auto_Clicker {
+    public partial class Form1 : Form {
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event (
+        int dwFlags,
+        int dx,
+        int dy,
+        int dwData,
+        int dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey (IntPtr hWnd, int id, int fsModifiers, int vlc);
+
+        public int index = 9999;
+        public bool selectata = false;
+
+        protected override void WndProc (ref Message m) {
+            if(m.Msg == 786 ) {
+                int da = m.WParam.ToInt32();
+                if ( da == 0 && index != 9999 )
+                    timer1.Start();
+                if ( da == 1 )
+                    timer1.Stop();
+            }
+            base.WndProc(ref m);
+        }
+
+
+        public void rightClick () {
+            mouse_event(8, 0, 0, 0, 0);
+            mouse_event(16, 0, 0, 0, 0);
+        }
+
+        public void leftClick () {
+            mouse_event(2, 0, 0, 0, 0);
+            mouse_event(4, 0, 0, 0, 0);
+        }
+
+
+
+        public Form1 () {
+            InitializeComponent();
+            int id = 0;
+            RegisterHotKey(this.Handle, id, 0, 120);
+            RegisterHotKey(this.Handle, id + 1, 0, 121);
+        }
+
+        private void Form1_Load (object sender, EventArgs e) {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged (object sender, EventArgs e) {
+            if ( CB_list.SelectedItems.Count == 1 ) {
+                selectata = true;
+                index = CB_list.SelectedIndex;
+            }
+            if ( CB_list.SelectedItems.Count != 0 )
+                return;
+            selectata = false;
+            index = 9999;
+        }
+
+        private void btn_start_Click (object sender, EventArgs e) {
+            if ( index == 9999 )
+                return;
+            timer1.Start();
+        }
+
+        private void btn_stop_Click (object sender, EventArgs e) {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick (object sender, EventArgs e) {
+            if ( index == 0 )
+                leftClick();
+            else if ( index == 1 )
+                rightClick();
+            Thread.Sleep(1);
+        }
+
+    }
+}
